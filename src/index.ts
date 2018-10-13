f631f638ec583ece889c5f1d366175bdf98b6a70
edouardbozon <bozonedouard@gmail.com>
2018-10-13 11:43:21 +0200

    Add sass support

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ import { html, render } from "lit-html";
 const helloTemplate = (name: string) => html`<div>Hello ${name}!</div>`;
 
 // This updates to <div>Hello Kevin!</div>, but only updates the ${name} part
-export default render(helloTemplate("Kevin"), document.body);
+export default render(helloTemplate("Kevisssn"), document.body);
