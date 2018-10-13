2a4c67ef3fb5bc43a7f9aad4bb7ca86cbbf27b19
edouardbozon <bozonedouard@gmail.com>
2018-10-13 11:45:31 +0200

    Add css support, normalize style, add nav layout

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { html, render } from "lit-html";
+import "normalize.css";
+import "./assets/index.scss";
+import { render } from "lit-html";
 
-// This is a lit-html template function. It returns a lit-html template.
-const helloTemplate = (name: string) => html`<div>Hello ${name}!</div>`;
+import home from "./app/home";
 
-// This updates to <div>Hello Kevin!</div>, but only updates the ${name} part
-export default render(helloTemplate("Kevisssn"), document.body);
+export default render(home(), document.body);
