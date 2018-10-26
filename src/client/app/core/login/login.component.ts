d578b659a7184977acb4ae120541a3479f17666c
edouardbozon <bozonedouard@gmail.com>
2018-10-26 11:00:55 +0200

    Add signup form

--- src/client/app/core/login/login.component.ts
+++ src/client/app/core/login/login.component.ts
@@ -5,20 +5,32 @@ import router from "../../../app-router";
 import { showError } from "../../utils/show-error";
 import _fetch from "../../utils/fetch";
 import { LoginPayload } from "../../../../server/api/auth/helpers/login-payload";
+import { SignupPayload } from "../../../../server/api/auth/helpers/signup-payload";
 
 export default class Login extends LitElement {
-  logUser = async (credentials: LoginPayload): Promise<any> => {
-    return await _fetch(`http://localhost:8081/api/v1/login`, {
+  showSignup = false;
+
+  async logUser(credentials: LoginPayload): Promise<string> {
+    const resp = await _fetch(`http://localhost:8081/api/v1/auth/login`, {
       method: "POST",
       mode: "cors",
       cache: "no-cache",
       body: JSON.stringify({ ...credentials }),
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
     });
-  };
+
+    return resp.json();
+  }
+
+  async signupUser(user: SignupPayload): Promise<{ user: any; token: string }> {
+    const resp = await _fetch(`http://localhost:8081/api/v1/auth/signup`, {
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      body: JSON.stringify({ ...user }),
+    });
+
+    return resp.json();
+  }
 
   render(): TemplateResult {
     return html`
@@ -36,28 +48,82 @@ export default class Login extends LitElement {
         }
       </style>
       <ez-page>
-        <h1>Login</h1>
-        <form name="login" @submit=${async (e: Event) => {
-          e.preventDefault();
+        <button @click=${() => {
+          this.showSignup = !this.showSignup;
+          this.update(new Map());
+        }}>Signup</button>
+        ${
+          this.showSignup
+            ? html`
+              <h1>Signup</h1>
+              <form name="signup" @submit=${async (e: Event) => {
+                e.preventDefault();
 
-          const host = this.shadowRoot as ShadowRoot;
-          const email = host.getElementById("email") as HTMLInputElement;
-          const password = host.getElementById("password") as HTMLInputElement;
-          const credentials = { email: email.value, password: password.value };
+                const host = this.shadowRoot as ShadowRoot;
+                const email = host.getElementById("email") as HTMLInputElement;
+                const password = host.getElementById(
+                  "password",
+                ) as HTMLInputElement;
+                const firstName = host.getElementById(
+                  "firstName",
+                ) as HTMLInputElement;
+                const lastName = host.getElementById(
+                  "lastName",
+                ) as HTMLInputElement;
+                const user = {
+                  email: email.value,
+                  password: password.value,
+                  firstName: firstName.value,
+                  lastName: lastName.value,
+                };
 
-          try {
-            await this.logUser(credentials);
-            router.push("/admin");
-          } catch (e) {
-            showError(e);
-          }
-        }}>
-          <label for="name">Name</label>
-          <input id="name" name="email" type="email" required />
-          <label for="password">Password</label>
-          <input id="password" name="password" type="password" required />
-          <button type="submit">Login</button>
-        </form>
+                try {
+                  await this.signupUser(user);
+                  router.push("/admin");
+                } catch (e) {
+                  showError(e);
+                }
+              }}>
+                <label for="email">Email</label>
+                <input id="email" name="email" type="email" required />
+                <label for="firstName">First name</label>
+                <input id="firstName" name="firstName" type="string" required />
+                <label for="lastName">Last name</label>
+                <input id="lastName" name="lastName" type="string" required />
+                <label for="password">Password</label>
+                <input id="password" name="password" type="password" required />
+                <button type="submit">Signup</button>
+              </form>`
+            : html`
+              <h1>Login</h1>
+              <form name="login" @submit=${async (e: Event) => {
+                e.preventDefault();
+
+                const host = this.shadowRoot as ShadowRoot;
+                const email = host.getElementById("email") as HTMLInputElement;
+                const password = host.getElementById(
+                  "password",
+                ) as HTMLInputElement;
+                const credentials = {
+                  email: email.value,
+                  password: password.value,
+                };
+
+                try {
+                  await this.logUser(credentials);
+                  router.push("/admin");
+                } catch (e) {
+                  showError(e);
+                }
+              }}>
+                <label for="email">Email</label>
+                <input id="email" name="email" type="email" required />
+                <label for="password">Password</label>
+                <input id="password" name="password" type="password" required />
+                <button type="submit">Login</button>
+              </form>
+          `
+        }
       </ez-page>
     `;
   }
