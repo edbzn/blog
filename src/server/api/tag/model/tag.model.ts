bdd4d8bbeff2cc14fdfe8356ded3ab08eab5306c
edouardbozon <bozonedouard@gmail.com>
2018-10-28 10:00:48 +0100

    Remove tag model, use flatten model

--- src/server/api/tag/model/tag.model.ts
+++
@@ -1,9 +0,0 @@
-import { prop, Typegoose } from "typegoose";
-import { MongooseDocument } from "mongoose";
-
-export type TagDocument = Tag & MongooseDocument;
-
-export class Tag extends Typegoose {
-  @prop({ required: true })
-  name: string = "";
-}
