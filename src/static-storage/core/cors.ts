4785aed55331999e66c1114c0f29d67981c871c6
edouardbozon <bozonedouard@gmail.com>
2018-10-28 18:08:09 +0100

    Improve static CORS headers

--- src/static-storage/core/cors.ts
+++ src/static-storage/core/cors.ts
@@ -1,8 +1,17 @@
 import * as http from "http";
 
 export function allowCORSRequest(response: http.ServerResponse): void {
-  response.setHeader("Access-Control-Allow-Origin", "*");
-  response.setHeader("Access-Control-Request-Method", "*");
-  response.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST");
-  response.setHeader("Access-Control-Allow-Headers", "*");
+  response.setHeader("Access-Control-Allow-Origin", [
+    process.env.FRONT_APP_DOMAIN || "http://localhost:8080",
+  ]);
+  response.setHeader("Access-Control-Allow-Headers", [
+    "Content-Type",
+    "Origin",
+    "Authorization",
+  ]);
+  response.setHeader("Access-Control-Allow-Methods", [
+    "OPTIONS",
+    "GET",
+    "POST",
+  ]);
 }
