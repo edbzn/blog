baa795b8b1224c5770c877d28472dfd7b6063c36
edouardbozon <bozonedouard@gmail.com>
2018-11-10 08:46:48 +0100

    Improve static HTML

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -36,13 +36,16 @@ module.exports = merge(devConfig, {
       title: "Codamit",
       template: path.resolve(__dirname, "src", "client", "index.html"),
       minify: true,
+      hash: true,
     }),
     new webpack.DefinePlugin({
       "process.env": {
         NODE_ENV: JSON.stringify("production"),
         API_BASE_URL: JSON.stringify("https://api.codamit.com"),
         STATIC_BASE_URL: JSON.stringify("https://static.codamit.com"),
-        SENTRY_DSN: JSON.stringify("https://47dc0f3309cb456b874afc36d3df16aa@sentry.io/1316181")
+        SENTRY_DSN: JSON.stringify(
+          "https://47dc0f3309cb456b874afc36d3df16aa@sentry.io/1316181",
+        ),
       },
     }),
   ],
