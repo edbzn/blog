9deda11dd660016f1a414bd997075a861511e7b8
edouardbozon <bozonedouard@gmail.com>
2018-11-10 10:11:04 +0100

    Add twitter action icons

--- src/client/app/core/home/twitter-feed.ts
+++ src/client/app/core/home/twitter-feed.ts
@@ -5,6 +5,8 @@ import _fetch from "../../utils/fetch";
 import { apiClient } from "../api";
 import { unsafeHTML } from "lit-html/directives/unsafe-html";
 import { timeSince } from "../../utils/time-since";
+import retweet from "../../utils/icons/retweet";
+import like from "../../utils/icons/like";
 
 const getTweets = async (): Promise<{ statuses: any[] }> => {
   return apiClient.get<{ statuses: any[] }>("/api/v1/tweet");
@@ -17,15 +19,26 @@ const showTweets = (resp: any) => {
         <article class="media">
           <div class="media-content">
             <div class="content">
-              <p>
+              <header>
                 <strong>${tweet.user.name}</strong>
-                <a href="https://twitter.com/${tweet.user.screen_name.toLowerCase()}">
+                <a
+                  href="https://twitter.com/${
+                    tweet.user.screen_name.toLowerCase()
+                  }"
+                >
                   <small>@${tweet.user.screen_name.toLowerCase()}</small>
-                </a> 
+                </a>
                 - <small>${timeSince(new Date(tweet.created_at))} ago</small>
-                <br>
-                ${unsafeHTML(tweet.text)}
-              </p>
+              </header>
+              <div class="content">${unsafeHTML(tweet.text)}</div>
+              <footer>
+                <span>
+                  <i class="heart">${like}</i> ${tweet.favorite_count}
+                </span>
+                <span>
+                  <i class="retweet">${retweet}</i> ${tweet.retweet_count}
+                </span>
+              </footer>
             </div>
           </div>
         </article>
@@ -36,20 +49,42 @@ const showTweets = (resp: any) => {
 
 export const twitterFeed = () => {
   return html`
-    <link href="assets/css/bulma.min.css" rel="stylesheet">
-    <style scoped>.uppercase { text-transform: uppercase; }</style>
+    <link href="assets/css/bulma.min.css" rel="stylesheet" />
+    <style scoped>
+      .uppercase {
+        text-transform: uppercase;
+      }
+      .retweet svg,
+      .heart svg {
+        width: 18px;
+      }
+
+      .retweet svg {
+        fill: #2dae5a;
+      }
+
+      .heart svg {
+        fill: #df3e3e;
+      }
+
+      footer span {
+        margin-right: 10px;
+      }
+    </style>
     <section class="section">
       <h4 class="subtitle uppercase">tweets</h4>
-      ${until(
-        getTweets().then(resp => showTweets(resp)),
-        placeholder({
-          count: 4,
-          minLines: 1,
-          maxLines: 3,
-          box: true,
-          image: false,
-        }),
-      )}
+      ${
+        until(
+          getTweets().then(resp => showTweets(resp)),
+          placeholder({
+            count: 4,
+            minLines: 1,
+            maxLines: 3,
+            box: true,
+            image: false,
+          }),
+        )
+      }
     </section>
   `;
 };
