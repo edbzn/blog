b37a2c8ef23c2b958c97f7dd8fdf517a8ff676b3
edouardbozon <bozonedouard@gmail.com>
2018-11-16 12:06:36 +0100

    Remove static storage

--- src/static-storage/core/public-path.ts
+++
@@ -1,9 +0,0 @@
-import { resolve } from "path";
-
-export const PUBLIC_PATH = process.env.production
-  ? resolve(__dirname, "data") // @todo find better location (it will throw)
-  : "/tmp/";
-
-export function getPublicPath(filename: string): string {
-  return PUBLIC_PATH + filename;
-}
