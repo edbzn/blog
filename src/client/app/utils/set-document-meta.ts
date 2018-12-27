9bd01f2f3cd13458bdb853253577369a5631be81
Koala <bozonedouard@gmail.com>
2018-12-27 11:26:16 +0100

    Add env in doc title

--- src/client/app/utils/set-document-meta.ts
+++ src/client/app/utils/set-document-meta.ts
@@ -2,19 +2,24 @@ export function setTitleAndMeta(
   title: string,
   description?: string | undefined,
 ): void {
-  document.title = title.length > 55 ? title.slice(0, 55) + "..." : title;
+  setDocTitle(title);
 
   if (description) {
     let meta = document.getElementsByTagName("meta");
+
     for (let i = 0; i < meta.length; i++) {
-      if (meta[i].name.toLowerCase().includes("description")) {
-        if (meta.item(i) !== null) {
-          (meta.item(i) as HTMLMetaElement).setAttribute(
-            "content",
-            description,
-          );
-        }
+      if (
+        meta[i].name.toLowerCase().includes("description") &&
+        meta.item(i) !== null
+      ) {
+        (meta.item(i) as HTMLMetaElement).setAttribute("content", description);
       }
     }
   }
 }
+
+const setDocTitle = (title: string): void => {
+  const prefix = process.env.NODE_ENV !== "production" ? "[DEV] - " : "";
+  document.title =
+    prefix + (title.length > 55 ? title.slice(0, 55) + "..." : title);
+};
