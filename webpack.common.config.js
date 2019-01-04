2ceb01d990c9571ed459b8fdfee2a5697748e2d5
edouardbozon <bozonedouard@gmail.com>
2019-01-04 13:01:23 +0100

    Fix webpack copy plugin inclusion

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const WriteFilePlugin = require("write-file-webpack-plugin");
+
 
 module.exports = {
   entry: "./src/client/index",
@@ -54,4 +57,41 @@ module.exports = {
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist", "client"),
   },
+  plugins: [
+    new WriteFilePlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: "node_modules/@webcomponents/webcomponentsjs",
+        to: "assets/js/webcomponentsjs",
+      },
+      {
+        from: "node_modules/bulma/css/bulma.min.css",
+        to: "assets/css/bulma.min.css",
+      },
+      {
+        from: "node_modules/simplemde/src/css/simplemde.css",
+        to: "assets/css/simplemde.css",
+      },
+      {
+        from: "node_modules/simplemde/debug/simplemde.css",
+        to: "assets/css/debug-simplemde.css",
+      },
+      {
+        from: "src/client/assets",
+        to: "assets",
+      },
+      {
+        from: "src/client/favicon.ico",
+      },
+      {
+        from: "src/client/robots.txt",
+      },
+      {
+        from: "src/client/manifest.json",
+      },
+      {
+        from: "src/client/sw.js",
+      },
+    ]),
+  ]
 }
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,7 +1,5 @@
 const path = require("path");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const WriteFilePlugin = require("write-file-webpack-plugin");
 const merge = require("webpack-merge");
 const Dotenv = require('dotenv-webpack');
 const common = require('./webpack.common.config');
@@ -15,41 +13,6 @@ module.exports = merge(common, {
     new Dotenv({
       path: './.env'
     }),
-    new WriteFilePlugin(),
-    new CopyWebpackPlugin([
-      {
-        from: "node_modules/@webcomponents/webcomponentsjs",
-        to: "assets/js/webcomponentsjs",
-      },
-      {
-        from: "node_modules/bulma/css/bulma.min.css",
-        to: "assets/css/bulma.min.css",
-      },
-      {
-        from: "node_modules/simplemde/src/css/simplemde.css",
-        to: "assets/css/simplemde.css",
-      },
-      {
-        from: "node_modules/simplemde/debug/simplemde.css",
-        to: "assets/css/debug-simplemde.css",
-      },
-      {
-        from: "src/client/assets",
-        to: "assets",
-      },
-      {
-        from: "src/client/favicon.ico",
-      },
-      {
-        from: "src/client/robots.txt",
-      },
-      {
-        from: "src/client/manifest.json",
-      },
-      {
-        from: "src/client/sw.js",
-      },
-    ]),
   ],
   devServer: {
     historyApiFallback: true,
