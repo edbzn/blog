5e25259528267c93744b0847cdc37e5b1825890a
edouardbozon <bozonedouard@gmail.com>
2019-02-24 13:08:06 +0100

    Add bundle analyzer plugin

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,9 +3,11 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const merge = require("webpack-merge");
 const Dotenv = require('dotenv-webpack');
 const common = require('./webpack.common.config');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = merge(common, {
   plugins: [
+    new BundleAnalyzerPlugin(),
     new HtmlWebpackPlugin({
       title: "Codamit [WDS] - [HMR]",
       template: path.resolve(__dirname, "src", "client", "index.dev.html"),
