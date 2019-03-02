c55c99629dfa307da598e36c8e47bc26ce5609e3
edouardbozon <bozonedouard@gmail.com>
2019-03-02 15:38:19 +0100

    Add runtime cache library to manage sw

---
+++ src/client/sw-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  staticFileGlobs: ['dist/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'],
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/api\.codamit\/.com/,
+      handler: "fastest",
+    },
+    {
+      urlPattern: /^https:\/\/codamit\/.com/,
+      handler: "fastest",
+    },
+  ],
+};
