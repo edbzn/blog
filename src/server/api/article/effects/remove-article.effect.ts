d5eab2efe7d12d43009be1817e6592969ca3afca
Koala <bozonedouard@gmail.com>
2019-04-26 12:10:00 +0200

    Fix remove effect

--- src/server/api/article/effects/remove-article.effect.ts
+++ src/server/api/article/effects/remove-article.effect.ts
@@ -3,14 +3,12 @@ import { requestValidator$, t } from '@marblejs/middleware-io';
 import { of, throwError } from 'rxjs';
 import { catchError, map, mergeMap } from 'rxjs/operators';
 
-import { articleSchema } from '../helpers/article-body.validator';
 import { ArticleDao } from '../model/article.dao';
 
 const validator$ = requestValidator$({
   params: t.type({
     id: t.string,
   }),
-  body: articleSchema,
 });
 
 export const removeArticleEffect$: HttpEffect = req$ =>
