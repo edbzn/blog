c97b7c040e1fb2eddab74d809c709460d6d65752
Koala <bozonedouard@gmail.com>
2019-04-28 00:18:16 +0200

    Fix slug and reactions

--- src/server/api/article/helpers/article-body.validator.ts
+++ src/server/api/article/helpers/article-body.validator.ts
@@ -2,17 +2,36 @@ import { requestValidator$, t } from '@marblejs/middleware-io';
 
 import { ArticleLanguage } from '../model/article-language';
 
+interface PositiveBrand {
+  readonly Positive: unique symbol;
+}
+
+const Positive = t.brand(
+  t.number,
+  (n): n is t.Branded<number, PositiveBrand> => n >= 0,
+  'Positive'
+);
+
+type Positive = t.TypeOf<typeof Positive>;
+
 export const articleSchema = t.type({
   title: t.string,
   markdown: t.string,
   html: t.string,
   tags: t.array(t.string),
-  slug: t.union([t.string, t.null]),
+  slug: t.string,
   published: t.boolean,
   publishedAt: t.union([t.string, t.null]),
   posterUrl: t.union([t.string, t.null]),
   metaTitle: t.union([t.string, t.null]),
   metaDescription: t.union([t.string, t.null]),
+  reactions: t.type({
+    types: t.type({
+      unicorn: t.type({ count: Positive }),
+      heart: t.type({ count: Positive }),
+      mark: t.type({ count: Positive }),
+    }),
+  }),
   lang: t.union([t.literal(ArticleLanguage.FR), t.literal(ArticleLanguage.EN)]),
 });
 
