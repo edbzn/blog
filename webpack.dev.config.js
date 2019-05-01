1fd5207db03546562a4fc48960601253214dd953
Koala <bozonedouard@gmail.com>
2019-05-01 13:53:41 +0200

    Remove hot reload and analyzer

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
 const Dotenv = require('dotenv-webpack');
 const common = require('./webpack.common.config');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const WebpackNotifierPlugin = require('webpack-notifier');
 
 module.exports = merge(common, {
@@ -20,10 +19,6 @@ module.exports = merge(common, {
         'icon-96x96.png'
       ),
     }),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: true,
-      analyzerPort: 8082,
-    }),
     new HtmlWebpackPlugin({
       title: 'Codamit [WDS] - [HMR]',
       template: path.resolve(__dirname, 'src', 'client', 'index.dev.html'),
@@ -36,6 +31,6 @@ module.exports = merge(common, {
   devServer: {
     historyApiFallback: true,
     compress: true,
-    hot: true,
+    hot: false,
   },
 });
