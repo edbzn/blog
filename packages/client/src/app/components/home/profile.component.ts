84383d3cb721c48190baf2fb69a8c4bf674bcd08
Koala <bozonedouard@gmail.com>
2019-06-27 12:02:10 +0200

    Add three scene

--- packages/client/src/app/components/home/profile.component.ts
+++ packages/client/src/app/components/home/profile.component.ts
@@ -1,14 +1,28 @@
 import { css, html, LitElement } from 'lit-element';
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  BoxGeometry,
+  MeshBasicMaterial,
+  Mesh,
+} from 'three';
 
 import { translate } from '../../core/directives/translate.directive';
 
 export default class ProfileComponent extends LitElement {
   static get styles() {
     return css`
+      :host {
+        display: block;
+        position: relative;
+      }
+
       section {
         padding: 5rem 0;
         margin-bottom: 42px;
-        background: #eee;
+        color: #fff;
+        z-index: 1;
         font-family: 'IBM Plex Sans', sans-serif;
       }
 
@@ -33,12 +47,53 @@ export default class ProfileComponent extends LitElement {
         font-weight: 100;
       }
 
+      .scene {
+        width: 100%;
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        z-index: -1;
+        background: #;
+      }
+
       @media screen and (max-width: 800px) {
         .page-wrapper {
           padding: 1rem 0.8rem;
         }
       }
     `;
+  }
+
+  firstUpdated() {
+    const scene = new Scene();
+    const { height } = this.shadowRoot!.host.getBoundingClientRect();
+    const camera = new PerspectiveCamera(75, window.innerWidth / height, 0.1, 1000);
+    const renderer = new WebGLRenderer();
+    const { domElement } = renderer;
+
+    domElement!.classList.add('scene');
+
+    renderer.setSize(window.innerWidth, height);
+    this.shadowRoot!.prepend(renderer.domElement);
+
+    const geometry = new BoxGeometry(1, 1, 1);
+    const material = new MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new Mesh(geometry, material);
+
+    scene.add(cube);
+
+    camera.position.z = 5;
+
+    const animate = () => {
+      renderer.render(scene, camera);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      requestAnimationFrame(animate);
+    };
+
+    animate();
   }
 
   render() {
