a7b2c06e063084cd6ea2b28c324bff6de9d2cc6d
Edouard Bozon <bozonedouard@gmail.com>
2020-11-27 10:41:34 +0100

    Fix 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
 import Layout from '../components/layout';
 import { SEO } from '../components/seo';
@@ -18,3 +19,13 @@ class NotFoundPage extends React.Component {
 }
 
 export default NotFoundPage;
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
