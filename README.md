2616d5e8c10545ae76fa6ff158cb16f010c9b520
Edouard Bozon <bozonedouard@gmail.com>
2024-03-28 12:07:10 +0100

    rm -rf *

--- README.md
+++
@@ -1,5 +0,0 @@
-# www.codamit.dev
-
-![Codamit](https://media.giphy.com/media/3o6Mb5ZXk5gzKggqBy/giphy.gif)
-
-[Browse to the site](https://www.codamit.dev)
--- gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-import 'typeface-merriweather';
-import 'typeface-montserrat';
-
-import './static/css/code-theme.css';
-import './static/css/main.css';
-
-export const onServiceWorkerUpdateReady = () => {
-  window.location.reload();
-};
